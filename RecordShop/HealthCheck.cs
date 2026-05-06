acfd5b16a07525b15dca44975d4627bdb351fdaf
michh18 <michellechan0518@gmail.com>
2026-05-06 17:14:01 +0100

    feat: to check api health status at /api/health endpoint

---
+++ RecordShop/HealthCheck.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace RecordShop
+{
+    // Source - https://stackoverflow.com/a/76481253
+    // Posted by mostafa, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-05-06, License - CC BY-SA 4.0
+
+    public class DbContextHealthCheck<TContext> : IHealthCheck where TContext : DbContext
+    {
+        private readonly TContext _dbContext;
+
+        public DbContextHealthCheck(TContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await _dbContext.Database.CanConnectAsync(cancellationToken);
+                return HealthCheckResult.Healthy();
+            }
+            catch (Exception ex)
+            {
+                return HealthCheckResult.Unhealthy("DbContext connection test failed", ex);
+            }
+        }
+    }
+
+}
