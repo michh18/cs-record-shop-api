2e7f1909addddd26a1cf0a30badad4cdc811bc25
michh18 <michellechan0518@gmail.com>
2026-05-06 17:26:16 +0100

    feat: allows for SQL server connection instead of InMemoryDatabase

--- RecordShop/Program.cs
+++ RecordShop/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using RecordShop.DataModels;
 using RecordShop.Repositories;
 using RecordShop.Services;
@@ -11,8 +12,13 @@ namespace RecordShop
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddDbContext<RecordShopDbContext>(options => options.UseInMemoryDatabase("RecordShopDb"));
+            // To use in-memory database
+            //builder.Services.AddDbContext<RecordShopDbContext>(options => options.UseInMemoryDatabase("RecordShopDb"));
+
+            builder.Services.AddDbContext<RecordShopDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddHealthChecks().AddCheck<DbContextHealthCheck<RecordShopDbContext>>("record_shop_health_check");
 
             builder.Services.AddControllers();
             builder.Services.AddScoped<IAlbumsRepository, AlbumsRepository>();
@@ -23,20 +29,22 @@ namespace RecordShop
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+            app.UseRouting();
+            app.MapHealthChecks("/api/health");
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            //}
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
             app.MapControllers();
-
+            
             app.Run();
         }
     }
